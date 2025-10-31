b059451f3bd4925639ad6adc1980837df7a1f70a
Raunit Jaiswal <raunitjaiswal510@gmail.com>
2025-10-31 22:28:52 +0530

    y first project as I dive into the harder concepts of Rust

---
+++ src/bin/main.rs
@@ -0,0 +1,56 @@
+use server::ThreadPool;
+use std::io::prelude::*;
+use std::net::{TcpListener, TcpStream};
+use std::time::Duration;
+use std::{fs, thread};
+
+fn main() {
+    let listener = match TcpListener::bind("127.0.0.1:8080") {
+        Ok(l) => l,
+        Err(e) => {
+            eprintln!("Failed to bind to 127.0.0.1:8080: {}", e);
+            std::process::exit(1);
+        }
+    };
+
+    let pool = ThreadPool::new(4);
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                pool.execute(|| {
+                    handle_connection(stream);
+                });
+            }
+            Err(e) => {
+                eprintln!("Connection failed: {}", e);
+            }
+        }
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer).unwrap();
+    let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
+
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK", "index.html")
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(5));
+        ("HTTP/1.1 200 OK", "index.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
+
+    let contents = fs::read_to_string(filename).unwrap();
+    let response = format!(
+        "{}\r\nContent-Length: {}\r\n\r\n{}",
+        status_line,
+        contents.len(),
+        contents
+    );
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+}
